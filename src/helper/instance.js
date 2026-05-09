b7e8b99aa64bf85494c52b873f3d055f7d5b2d82
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2026-05-09 23:31:07 +0530

    lit cng

--- src/helper/instance.js
+++ src/helper/instance.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
-export let url;
-url =
-  import.meta.env.MODE === 'development'
-    ? import.meta.env.VITE_API_BACKEND_URL
-    : import.meta.env.VITE_PREFIX_URL;
+let url =import.meta.env.VITE_API_BACKEND_URL;
 
 const instance = axios.create({
   baseURL: url,
